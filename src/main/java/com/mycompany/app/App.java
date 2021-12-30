d1b27539916e18c40fbc152ea2774a0bb72487e8
nagenn <44678680+nagenn@users.noreply.github.com>
2021-12-30 13:44:05 +0530

    Intentionally breaking the code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello -- World!";
 
     public App() {}
 
@@ -15,7 +15,7 @@ public class App
     }
     
     public static int calc(int a, int b) {
-        int result1 = a * b;
+        int result1 = a + b;
         return  result1;
     }
     
